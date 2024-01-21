c8ed679049bd8f6175dcd2660cdbc72550acfe30
zskbombon <145676897+zskbombon@users.noreply.github.com>
2024-01-21 22:52:31 +0100

    Add files via upload

---
+++ cshark/cshark/gi/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace gsn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] T;
+            string klasa;
+            int klasaint, lvl, nlvl, exp, monety, hp, zre, sila=1, bron=0;
+//level, kasa, exp do zrobienia 
+            do
+                {
+                    Console.WriteLine("wybierz klase:\n1. Samurai\n2. Ninja");
+                    klasa = Console.ReadLine();
+                    klasaint = Convert.ToInt32(klasa);
+                }while(klasaint != 1 && klasaint != 2);
+
+            if(klasaint==1)
+                {
+                    klasa="samuraj";
+                    hp=200;
+                    bron=10;
+                    zre=2;
+                }
+            else
+                {
+                    klasa="ninja";
+                    hp=100;
+                    bron=5;
+                    zre=5;
+                }
+            Console.WriteLine("twoja klasa to: " + klasa + "\n twoje statystyki to:\n" + hp + "\n" + bron + "\n" + zre);
+
+            System.Console.WriteLine();
+            Console.WriteLine("zadajesz " + dmg(sila, bron));
+
+
+           
+        }
+/*
+        static void walka(int dmg, )
+        {
+
+            
+
+        }
+*/
+        static int dmg(int sila, int bron)
+        {
+            Random generator = new Random();
+            return sila * bron* generator.Next(11);
+        }
+    
+
+    }
+}
---
+++ cshark/kryptoinator/kryptoinator2000/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace krypto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //cezar();
+            //slowny();
+            //poli();
+            //atbasz();
+            vigenera();
+
+        }
+
+        static void cezar()
+        {
+            string czyste, alfabet = "abcdefghijklmnopqrstuvwxyz";
+            int kod = 0;
+            
+            Console.WriteLine("Witaj, wpisz fraze ktora chcesz zakodowac");
+            czyste =Console.ReadLine();
+            czyste = czyste.ToLower();
+            
+            Console.WriteLine("podaj kod");
+            kod = Convert.ToInt32(Console.ReadLine());
+                
+            char[] cz_tab = czyste.ToCharArray();
+
+            int[] al_tab = new int[cz_tab.Length];
+
+            for (int i = 0; i < cz_tab.Length; i++)
+            {
+                al_tab[i] = alfabet.IndexOf(cz_tab[i]) + kod;
+                while(al_tab[i] > 25)
+                {
+                    al_tab[i]-=26;
+                }
+            }
+
+             for (int i = 0; i < al_tab.Length; i++)
+                {
+                    if(char.IsWhiteSpace(cz_tab[i]))
+                    {
+                        System.Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(alfabet[al_tab[i]]);
+                    }
+                }
+            
+        }
+    
+        static void slowny()
+        {
+            string czyste, kod;
+            
+            Console.WriteLine("podaj fraze ktora chcesz zakodowac");
+            czyste = Console.ReadLine();
+
+            Console.WriteLine("podaj kod(najlepiej parzysty bez powtarzajacych sie liter)");
+            kod = Console.ReadLine();
+
+            char[] czt = czyste.ToCharArray();
+
+            for(int i=0; i<czyste.Length; i++)
+            {
+                for(int j=0; j<kod.Length; j++)
+                {
+                    if(czyste[i]==kod[j])
+                    {
+                        if(j%2!=0)
+                        {
+                            czt[i]=kod[j-1];
+                        }
+                        else
+                        {
+                            czt[i]=kod[j+1];
+                        }
+                        break;
+                    }
+                }
+            }
+
+            System.Console.WriteLine(czt);
+
+        }
+    
+        static void poli()
+        {
+
+           char[,] tl = {{'a', 'b', 'c', 'd', 'e'}, {'f', 'g', 'h', 'i', 'j'}, {'k', 'l', 'm', 'n', 'o'}, {'p', 'r', 's', 't', 'u'}, {'v', 'w', 'x', 'y', 'z'}};
+
+           Console.WriteLine("podaj fraze ktora chcesz zakodowac");
+           string czyste = Console.ReadLine();
+
+
+           for (int i = 1; i < czyste.Length+1; i++)
+           {
+
+                for (int j = 1; j < 6; j++)
+                {
+
+                    for (int k = 1; k < 6; k++)
+                    {
+
+                        if(czyste[i-1] == tl[j-1, k-1])
+                        {
+
+                            Console.Write(j + "" + k + " ");
+
+                        }
+
+                    }
+
+                }
+
+           }
+
+        }
+
+        static void atbasz()
+        {
+            
+            string oa="zyxwvutsrqponmlkjihgfedcba", a="abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine("podaj fraze ktora chcesz zakodowac");
+            string czyste = Console.ReadLine();
+
+            for (int i = 0; i < czyste.Length; i++)
+            {
+
+                for (int j = 0; j < oa.Length; j++)
+                {
+
+
+                    if(czyste[i] == a[j])
+                    {
+
+                        Console.Write(oa[j]);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        static void vigenera()
+        {
+            string alfabet = "abcdefghijklmnopqrstuvwxyz";
+            
+            Console.WriteLine("podaj fraze ktora chcesz zakodowac");
+            string czyste=Console.ReadLine();
+
+            Console.WriteLine("podaj kod");
+            string kod=Console.ReadLine();
+
+            for (int i = 0; i < czys
+
+
+        }
+    }
+}
